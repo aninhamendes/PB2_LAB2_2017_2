475483da1c790114cbf4d731be4dbda4ea855390
Ana Paula Mendes <anamendest@outlook.com>
2017-12-06 20:54:19 -0300

    Remoção de convenções de Pluralização

---
+++ WebAppProva2Lab2AnaMendes/Models/AdvocaciaContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProva2Lab2AnaMendes.Models
+{
+    public class AdvocaciaContext : DbContext
+    {
+        public AdvocaciaContext() : base("name=AdvocaciaContext")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+        }
+
+        
+    }
+}
